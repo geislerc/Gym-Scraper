ed59c773d2dd7fa41a3663c139a153196d16c4fa
geislerc <geislerc@uwm.edu>
2024-08-06 23:33:15 -0500

    No longer adding another new row after each row input with csv

--- arrayToCSV.py
+++ arrayToCSV.py
@@ -7,13 +7,14 @@ def write2DArrToCSV(startArr: list, name: str, path: str = "") -> str:
         name = path + "/" + name
         directory = Path(name)
         directory.parent.mkdir(parents=True, exist_ok=True)
-    with open((name), "w+") as my_csv:
+    with open((name),  "w+", newline='') as my_csv:
         writer = csv.writer(my_csv, delimiter=',')
-        writer.writerows(startArr)
+        for row in startArr:
+            writer.writerow(row)
     return name
 
 
-# a = [[1, 2, 3, 4], [5, 6, 7, 8]]
-# filename = "Heck"
-# mypath = "jjj/sd/"
-# write2DArrToCSV(a, filename, mypath)
+a = [[1, 2, 3, 4], [5, 666, 7, 8]]
+filename = "Heck"
+mypath = "jjj/sd"
+write2DArrToCSV(a, filename, mypath)
