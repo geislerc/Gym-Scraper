4abda8e04dbb13d4a0be8023fc4bd3e014776547
geislerc <geislerc@uwm.edu>
2024-08-06 22:58:05 -0500

    Turned arrayToCSV.py to a function that can be used externally

--- arrayToCSV.py
+++ arrayToCSV.py
@@ -1,7 +1,20 @@
 import csv
 
-a = [[1,2,3,4],[5,6,7,8]]
 
-with open("new_file.csv","w+") as my_csv:
-    csvWriter = csv.writer(my_csv,delimiter=',')
-    csvWriter.writerows(a)
+# a = [[1,2,3,4],[5,6,7,8]]
+#
+# with open("new_file.csv","w+") as my_csv:
+#     csvWriter = csv.writer(my_csv,delimiter=',')
+#     csvWriter.writerows(a)
+
+def write2DArrToCSV(startArray, name):
+    name = name + ".csv"
+    with open((name), "w+") as my_csv:
+        writer = csv.writer(my_csv, delimiter=',')
+        writer.writerows(startArray)
+    return name
+
+
+# a = [[1,2,3,4],[5,6,7,8]]
+# filename = "Heck"
+# write2DArrToCSV(a, filename)
